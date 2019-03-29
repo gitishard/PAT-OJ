73ea9b09fb9214a547497c98466750752e7518c8
xiaoyuan <1099457325@qq.com>
2019-03-29 10:36:27 +0800

    new code

--- Joffer/reverseList.cpp
+++ Joffer/reverseList.cpp
@@ -1,25 +1,26 @@
-/**
-*  struct ListNode {
-*        int val;
-*        struct ListNode *next;
-*        ListNode(int x) :
-*              val(x), next(NULL) {
-*        }
-*  };
-*/
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
 class Solution {
 public:
-    vector<int> printListFromTailToHead(ListNode* head) {
-        vector<int> ary;
-        stack<int> st;
-        while(head != NULL){
-            st.push(head->val);
-            head = head->next;
+    ListNode* ReverseList(ListNode* pHead) {
+        vector<int> tmp;
+        ListNode* p = pHead;
+        while(p != NULL){
+            tmp.push_back(p->val);
+            p = p->next;
+         }
+        int len = tmp.size();
+        p = pHead;
+        while(len>=0 && p != NULL){
+            p->val = tmp[--len];
+            p = p->next;
         }
-        while(!st.empty()){
-            ary.push_back(st.top());
-            st.pop();
-        }
-        return ary;
+        return pHead;
     }
 };
